b7f1bfda24b32ce48dbdd32fd50fb63145df1717
Thedd <mixailmar4uk78@gmail.com>
2024-08-17 14:17:03 +0300

    added versel json

--- src/settings/apply-app-settings.ts
+++ src/settings/apply-app-settings.ts
@@ -4,8 +4,14 @@ import { apiPrefixSettings } from './app-prefix-settings';
 // Используем данную функцию в main.ts и в e2e тестах
 export const applyAppSettings = (app: INestApplication) => {
   setApiPrefix(app);
+
+  enableCors(app);
 };
 
 const setApiPrefix = (app: INestApplication) => {
   app.setGlobalPrefix(apiPrefixSettings.API_PREFIX);
+};
+
+const enableCors = (app: INestApplication) => {
+  app.enableCors();
 };
